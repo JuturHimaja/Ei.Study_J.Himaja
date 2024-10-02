0bc49a48ef0dc314af489e8194b85829b7093e66
JuturHimaja <himajajutur333@gmail.com>
2024-10-02 15:08:46 +0530

    Create Student.java

---
+++ ObserverPatternDemo/src/observers/Student.java
@@ -0,0 +1,14 @@
+package observers;
+
+public class Student implements IObserver {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void update(String message) {
+        System.out.println(name + " received notification: " + message);
+    }
+}
