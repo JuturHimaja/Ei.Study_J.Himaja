25abda7150f404b3d68968f3f2c2a2866398a85d
JuturHimaja <himajajutur333@gmail.com>
2024-10-02 16:06:21 +0530

    Delete ObserverPatternDemo directory

--- ObserverPatternDemo/src/observers/IObserver.java
+++
@@ -1,5 +0,0 @@
-package observers;
-
-public interface IObserver {
-    void update(String message);
-}
--- ObserverPatternDemo/src/observers/ISubject.java
+++
@@ -1,7 +0,0 @@
-package observers;
-
-public interface ISubject {
-    void attach(IObserver observer);
-    void detach(IObserver observer);
-    void notifyObservers();
-}
